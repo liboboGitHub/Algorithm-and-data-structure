43eb88e1590793ea9346046f702398bf46244bf1
李博博 <408536971@qq,com>
2019-12-09 16:50:02 +0800

    动态规划和记忆化搜素解决 0-1背包问题

---
+++ 动态规划/src/knapsack01_2.java
@@ -0,0 +1,39 @@
+/*
+ *使用动态规划来解决0-1背包问题
+ * */
+public class knapsack01_2 {
+
+    public int knapsack(int[] wight, int[] value, int Capacity) {
+        if (wight == null || value == null) {
+            return 0;
+        }
+        int n = wight.length;
+        int[][] memo = new int[n][Capacity + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < Capacity + 1; j++) {
+                memo[i][j] = -1;
+            }
+
+        }
+        assert (wight.length == value.length);
+
+        // 初始化子问题（选取第一个物品之后，在不同的背包容量情况下所获得的总价值）
+        for (int j = 0; j <= Capacity; j++) {
+            memo[0][j] = (j >= wight[0] ? value[0] : 0);
+        }
+
+        // 自底向上
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= Capacity; j++) {
+                memo[i][j] = memo[i - 1][j];  // 第一种策略，最后一个物品不放入背包
+                if (wight[i] <= j) {
+                    memo[i][j] = Math.max(memo[i][j], value[i] + memo[i - 1][j - wight[i]]);
+                }
+
+            }
+
+        }
+        return memo[n - 1][Capacity];
+
+    }
+}
